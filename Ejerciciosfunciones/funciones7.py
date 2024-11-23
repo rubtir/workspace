374559c2756c64f79efb19bbd26e7ba11bf7071f
rubtir <159425558+rubtir@users.noreply.github.com>
2024-11-23 14:01:08 +0100

    Update funciones7.py

---
+++ Ejerciciosfunciones/funciones7.py
@@ -0,0 +1,13 @@
+def MCD(num_1,num_2):
+    if num_1>num_2:
+        num1=num_1
+        num2=num_2
+    else:
+        num1=num_2
+        num2=num_1
+    while num2 != 0:
+        num2, num1 =num1%num2,num2
+    print(f'El máximo común divisor es: {num1}')
+num_1= int(input('Dime el primer número: '))
+num_2= int(input('Dime el segundo número: '))
+MCD(num_1,num_2)
