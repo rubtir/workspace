de2408369c7faba4830afea8e56e358b9a34a039
rubtir <159425558+rubtir@users.noreply.github.com>
2024-11-24 19:13:34 +0100

    sip

--- Ejerciciosfunciones/funciones7.py
+++ Ejerciciosfunciones/funciones7.py
@@ -5,9 +5,9 @@ def MCD(num_1,num_2):
     else:
         num1=num_2
         num2=num_1
-    while num2 != 0:
+    while num1%num2 != 0:
         num2, num1 =num1%num2,num2
-    print(f'El máximo común divisor es: {num1}')
+    print(f'El máximo común divisor es: {num2}')
 num_1= int(input('Dime el primer número: '))
 num_2= int(input('Dime el segundo número: '))
 MCD(num_1,num_2)
